84520a197b24ddecd3b3bd09458e788469a8b18e
Maria Aldana <maria.aldana@sinco.com.co>
2025-10-28 13:02:39 -0500

    🟦  Jugadores 3 puntos y ambos los mismos. Iguales

--- Tenis/TenisTest.cs
+++ Tenis/TenisTest.cs
@@ -54,7 +54,7 @@ public class TenisTest
             
             if (diferencia == 1) return "Ventaja jugador 1";
             if (diferencia == -1) return "Ventaja jugador 2";
-            if (diferencia == 0) return "Iguales";
+            return "Iguales";
         }
         return "";
     }
--- Tenis/obj/Debug/net8.0/Tenis.AssemblyInfo.cs
+++ Tenis/obj/Debug/net8.0/Tenis.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tenis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7a01a304f561154d226bad3c694cb07a3ad19d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee63975a8c63a5ac7b964cab480d987082c0eeb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tenis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tenis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
